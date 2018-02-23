a21368a9d5ba675d0b0ab58df1bf1a6ee23ca930
Yuya.Nishida <yuya@j96.org>
2018-02-23 19:43:22 +0900

    fix about codeclimate.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,11 +1,11 @@
-if ENV["CODECLIMATE_REPO_TOKEN"]
-  require "codeclimate-test-reporter"
-  CodeClimate::TestReporter.start
-end
-
 require "test/unit"
 
 require "stringio"
 require "logger"
 
+require "simplecov"
 require "mocha/setup"
+
+SimpleCov.start do
+  add_filter "/test/"
+end
